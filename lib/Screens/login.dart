30104c0cf5681966fb1c7d56ab0e2c579dc2f667
LDrago-zae <107863119+LDrago-zae@users.noreply.github.com>
2024-11-15 20:29:59 +0500

    update

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -36,6 +36,7 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< Updated upstream
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
@@ -117,6 +118,126 @@ class _LoginState extends State<Login> {
                           setState(() {
                             selectedCountryCode = newValue!;
                           });
+=======
+        body: SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(color: Color(0xff0e212d)),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 200),
+          child: Form(
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  "LOGIN",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: TextFormField(
+                      controller: email,
+                      textInputAction: TextInputAction.done,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).unfocus();
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(14))),
+                        labelText: "Email ",
+                        labelStyle:
+                            TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      cursorColor: Colors.white,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      validator: (String? txt) {
+                        if (txt == null || txt.isEmpty) {
+                          return "Enter your email";
+                        }
+                        return null;
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
+                  child: TextFormField(
+                      obscureText: true,
+                      obscuringCharacter: "*",
+                      controller: password,
+                      textInputAction: TextInputAction.done,
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context).unfocus();
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(14),
+                          ),
+                        ),
+                        labelText: "Password ",
+                        labelStyle:
+                            TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      cursorColor: Colors.white,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      validator: (String? txt) {
+                        if (txt == null || txt.isEmpty) {
+                          return "Enter your password";
+                        }
+
+                        return null;
+                      }),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: _login,
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF000000),
+                      foregroundColor: Colors.white,
+                      shadowColor: Colors.transparent),
+                  child: const Text(
+                    "Login",
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 90, top: 20),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Don't Have an Account?",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      InkWell(
+                        child: const Text(
+                          "SignUp",
+                          style: TextStyle(
+                              color: Colors.lightBlueAccent, fontSize: 14),
+                        ),
+                        onTap: () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return Signin();
+                          }));
+>>>>>>> Stashed changes
                         },
                         underline: const SizedBox(),
                         dropdownColor: Colors.grey.shade100,
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:rent_app/Screens/splash_screen.dart';
 
+<<<<<<< Updated upstream
 void main(){
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Splash(),
   ));
 }
+=======
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+    ),
+  );
+}
+>>>>>>> Stashed changes
